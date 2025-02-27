eac6ed91981f6d007bd43ea5830106fae227a923
Thierno2699 <94230185+Thierno2699@users.noreply.github.com>
2025-02-27 08:20:52 +0000

    update on sql

--- sql_scripts/init.sql
+++ sql_scripts/init.sql
@@ -3,8 +3,7 @@ USE staging_db;
 
 -- Table pour les films (modifiée)
 CREATE TABLE IF NOT EXISTS movies (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    film_title VARCHAR(255) NOT NULL,  -- Fusion name + year (ex: "Inception 2010")
+    film_title VARCHAR(255) PRIMARY KEY,  -- Clé primaire unique pour chaque film
     movie_rated VARCHAR(10),
     run_length VARCHAR(50),
     genres TEXT,
@@ -12,19 +11,20 @@ CREATE TABLE IF NOT EXISTS movies (
     rating DECIMAL(3,1),
     num_raters INT,
     num_reviews INT,
-    review_url VARCHAR(500),
-    UNIQUE KEY unique_movie (film_title) -- Empêche les doublons
+    review_url VARCHAR(500)
 );
 
--- Table pour les reviews (inchangée)
+-- Table pour les reviews (modifiée)
 CREATE TABLE IF NOT EXISTS reviews (
-    id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255),
+    film_title VARCHAR(255),
     rating DECIMAL(3,1),
     helpful INT,
     total INT,
     date VARCHAR(50),
     title TEXT,
     review TEXT,
-    film_title VARCHAR(255) 
+    PRIMARY KEY (username, film_title),  
+    FOREIGN KEY (film_title) REFERENCES movies(film_title) ON DELETE CASCADE
 );
+
